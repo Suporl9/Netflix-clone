a3790ce9dceaa17a21d8fac4eb4d4e14e5a99e61
Suman <sumanpok9@gmail.com>
2023-09-11 00:04:18 +0545

    add faker to gen random data for sign up

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,12 +1,19 @@
+import { faker } from '@faker-js/faker';
+
 describe('template spec', () => {
   it('passes', () => {
+    const userEmail = faker.internet.email();
+    const userPassword = faker.internet.password();
     cy.visit('http://localhost:3000/');
     cy.get('.signinbtn').contains('Sign In').click();
     cy.get('.sigup').contains('Sign up now').click();
 
-    cy.get('#users-input-email').type('sumanpok9@gmail.comd9dds909');
-    cy.get('input[placeholder="Password*"]').type('password');
-    cy.get('input[placeholder="Confirm Password*"]').type('password');
+    // cy.get('#users-input-email').type('sumanpok9@gmail.comd9dds909');
+    // cy.get('input[placeholder="Password*"]').type('password');
+
+    cy.get('#users-input-email').type(userEmail);
+    cy.get('input[placeholder="Password*"]').type(userPassword);
+    cy.get('input[placeholder="Confirm Password*"]').type(userPassword);
     // cy.get('.input-container').children('.linkbtn').click();
     cy.get('.linkbtn').click();
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,9 @@
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
+// import faker from 'faker';
+// import _ from 'lodash';
+
 //
 // For more comprehensive examples of custom
 // commands please read more here:
@@ -23,3 +26,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.add('generateFixtures', () => {
+//   cy.writeFile('cypress/fixtures/users.json', {
+//     hits: Cypress._.times(20, () => {
+//       return {
+//         email: faker.internet.email,
+//         password: faker.internet.password,
+//       };
+//     }),
+//   });
+// });
